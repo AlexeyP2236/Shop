00834a5213132b7c963990da52db47798efc07bf
AlexeyP2236 <129593056+AlexeyP2236@users.noreply.github.com>
2023-04-20 12:35:15 +0300

    Update Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -7,28 +7,28 @@ const Slider = () => {
       <Carousel.Item interval={1000}>
         <img height={800} width={'100%'}
           className="d-block w-100"
-          src="./img/Foto1 (1).jpg"
+          src="./img/Foto_(1).jpg"
           alt="First slide"
         />
       </Carousel.Item>
       <Carousel.Item interval={500}>
         <img height={800} width={'100%'}
           className="d-block w-100"
-          src="./img/Foto1 (2).jpg"
+          src="./img/Foto_(2).jpg"
           alt="Second slide"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img height={800} width={'100%'}
           className="d-block w-100"
-          src="./img/Foto1 (3).jpg"
+          src="./img/Foto_(3).jpg"
           alt="Third slide"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img height={800} width={'100%'}
           className="d-block w-100"
-          src="./img/Foto1 (4).jpg"
+          src="./img/Foto_(4).jpg"
           alt="Four slide"
         />
       </Carousel.Item>
